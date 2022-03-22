77850b61f8d87ec77c5a177e4e8ac151e3659d23
Netty <netty.pame@gmail.com>
2022-03-22 07:32:53 -0600

    agregamos platform

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
@@ -19,7 +20,9 @@ function setup(){
 
    
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150,305,300,170);
+
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -62,4 +65,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
